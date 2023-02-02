b7635d37ad85546f0bb49aef53a8194df20ccc77
filipton <filipton12@gmail.com>
2023-02-02 14:17:34 +0100

    feat: now frontend has dockerfile

--- frontend/arm-deploy.sh
+++ frontend/arm-deploy.sh
@@ -3,4 +3,4 @@
 rustup target add aarch64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu
 
-buildx build --platform linux/arm64 --tag filipton/keyracer-server:latest --push -f arm.Dockerfile .
+buildx build --platform linux/arm64 --tag filipton/keyracer-client:latest --push -f arm.Dockerfile .
